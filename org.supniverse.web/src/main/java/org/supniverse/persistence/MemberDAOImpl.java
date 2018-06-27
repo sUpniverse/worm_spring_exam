4389dfae83a7ba91500c9e63a76f5187bc4e7b67
김문섭 <kmsup2@gmail.com>
2018-06-27 22:32:23 +0900

    Mapper를 이용한 member 정보 조회 sql 작성

--- org.supniverse.web/src/main/java/org/supniverse/persistence/MemberDAOImpl.java
+++ org.supniverse.web/src/main/java/org/supniverse/persistence/MemberDAOImpl.java
@@ -1,5 +1,8 @@
 package org.supniverse.persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -24,6 +27,23 @@ public class MemberDAOImpl implements MemberDAO{
 	public void insertMember(MemberVO vo) {
 		// TODO Auto-generated method stub
 		sqlSession.insert(namespace+".insertMember",vo);		
+	}
+
+	@Override
+	public MemberVO readMember(String userid) throws Exception {
+		// TODO Auto-generated method stub
+		return (MemberVO)sqlSession.selectOne(namespace+".selectMember",userid);
+	}
+
+	@Override
+	public MemberVO readWithPW(String userid, String userpw) throws Exception {
+		// TODO Auto-generated method stub
+		Map<String, Object> paramMap = new HashMap<String,Object>();
+		
+		paramMap.put("userid", userid);
+		paramMap.put("userpw", userpw);
+		
+		return sqlSession.selectOne(namespace+".readWithPW",paramMap);
 	}
 
 }
