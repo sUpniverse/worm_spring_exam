8bc61d0595428461cd1c23fbe22514684faaf253
김문섭 <kmsup2@gmail.com>
2018-07-10 22:24:26 +0900

    검색처리를 위한 bar 추가 및 url 설정

---
+++ CRUD_exam/src/main/java/org/supniverse/controller/searchBoardController.java
@@ -0,0 +1,40 @@
+package org.supniverse.controller;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.supniverse.domain.PageMaker;
+import org.supniverse.domain.SearchCriteria;
+import org.supniverse.service.BoardService;
+
+@Controller
+@RequestMapping("/sboard/*")
+public class searchBoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(searchBoardController.class);
+	
+	@Inject
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void listPage(@ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception{
+		logger.info(cri.toString());
+		
+		model.addAttribute("list", service.listCriteria(cri));
+		
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		
+		pageMaker.setTotalCount(service.listCountCriteria(cri));
+		
+		model.addAttribute("pageMaker",pageMaker);
+		
+	}
+
+}
--- CRUD_exam/src/main/java/org/supniverse/domain/PageMaker.java
+++ CRUD_exam/src/main/java/org/supniverse/domain/PageMaker.java
@@ -1,5 +1,8 @@
 package org.supniverse.domain;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -49,6 +52,27 @@ public class PageMaker {
 				.build();
 		
 		return uriComponents.toUriString();
+	}
+	
+	public String makeSearch(int page) {
+		UriComponents uriComponents = UriComponentsBuilder.newInstance()
+				.queryParam("page", page)
+				.queryParam("perPageNum", cri.getPerPageNum())
+				.queryParam("searchType", ((SearchCriteria)cri).getSearchType())
+				.build();
+		return uriComponents.toUriString();
+	}
+	
+	private String encoding(String keyword) {
+		if(keyword == null || keyword.trim().length() == 0) {
+			return "";
+		}
+		
+		try {
+			return URLEncoder.encode(keyword, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}		
 	}
 
 	public int getTotalCount() {
