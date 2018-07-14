7ae7e82aa60b12caa7e00deef1d6472be2548d47
김문섭 <kmsup2@gmail.com>
2018-07-14 21:55:30 +0900

    Restful, Ajax 연습을 위한 프로젝트 추가

---
+++ Rest_Ajax_Exam/src/main/java/org/supniverse/controller/SampleController.java
@@ -0,0 +1,30 @@
+package org.supniverse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import domain.SampleVO;
+
+// RestController를 선언하면 모든 뷰 처리가 JSP가 아니라 것을 의 
+// ResponseBody를 선언하지않아도 동일하게 적용
+// 자동으로 JSON으로 처리된다.
+@RestController 
+@RequestMapping("/sample")
+public class SampleController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/sendVO")
+	public SampleVO sendVO() {
+		SampleVO vo = new SampleVO();
+		vo.setFirstName("길동");
+		vo.setLastName("홍");
+		vo.setMno(123);
+		
+		return vo;
+	}		
+}
